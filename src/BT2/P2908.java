c663d067a556698c111f415061921546ca00a773
sunbrun1 <dlwpghks1117@naver.com>
2022-06-28 20:53:48 +0900

    Bronze Tier 2 - 2908

---
+++ src/BT2/P2908.java
@@ -0,0 +1,27 @@
+package BT2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P2908 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
+		if(A > B) {
+			System.out.println(A);
+		}else {
+			System.out.println(B);			
+		}
+		
+
+	}
+
+}
