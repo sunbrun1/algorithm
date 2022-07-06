9be652c75684fca9e85eda5aedde79b70697c055
sunbrun1 <dlwpghks1117@naver.com>
2022-07-06 20:47:05 +0900

    Bronze Tier 3 - 5086

---
+++ src/BT3/P5086.java
@@ -0,0 +1,25 @@
+package BT3;
+
+import java.util.Scanner;
+
+public class P5086 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			int first = sc.nextInt();
+			int second = sc.nextInt();
+			
+			if(first == 0 && second == 0) {
+				break;
+			}
+			if(second % first == 0) {
+				System.out.println("factor");
+			}else if(first % second == 0) {
+				System.out.println("multiple");
+			}else {
+				System.out.println("neither");
+			}
+		}
+	}
+}
