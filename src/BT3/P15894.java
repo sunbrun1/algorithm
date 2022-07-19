2caa798801c0a74bc3a2f1915b7379a735c78e1e
sunbrun1 <dlwpghks1117@naver.com>
2022-07-19 20:24:19 +0900

    Bronze Tier 3 - 15894

---
+++ src/BT3/P15894.java
@@ -0,0 +1,15 @@
+package BT3;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P15894 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc.nextBigInteger().multiply( BigInteger.valueOf(4)) );
+		
+	}
+
+}
