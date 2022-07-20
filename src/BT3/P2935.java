ae7cb99d65925ca598bdd686253bbc1c41ed4ac7
sunbrun1 <dlwpghks1117@naver.com>
2022-07-20 20:09:41 +0900

    Bronze Tier 3 - 2935

---
+++ src/BT3/P2935.java
@@ -0,0 +1,22 @@
+package BT3;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P2935 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        BigInteger n = new BigInteger(sc.nextLine());
+        char c = sc.nextLine().charAt(0);
+        BigInteger m = new BigInteger(sc.nextLine());
+	
+        if(c == '*') {
+        	System.out.println(n.multiply(m));
+        }
+            
+        else {
+        	System.out.println(n.add(m));
+        }
+	}
+}
